c321747874e971bfb8b869af8fe804b33134dcec
Nonso <nonsootoh@gmail.com>
2023-09-26 19:51:15 -0400

    chore: adds practice questions

---
+++ javascript-starter/fruit_in_basket.js
@@ -0,0 +1,20 @@
+const fruit_in_basket = (lst) => {
+  /*
+    You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are
+    represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+    You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+
+    1. You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on    the amount of fruit each basket can hold.
+
+    2. Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+
+    3. Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+
+    Given the integer array fruits, return the maximum number of fruits you can pick.
+
+    console.log(fruit_in_basket([1,2,1])) -> 3
+    console.log(fruit_in_basket([0,1,2,2])) -> 3
+    console.log(fruit_in_basket([1,2,3,2,2])) -> 4
+*/
+  // implement
+};
---
+++ javascript-starter/longest_repeating_char_replace.js
@@ -0,0 +1,14 @@
+const longest_repeating_char_replace = (s, k) => {
+  /*
+    You are given a string s and an integer k. You can choose any character of the string and change it to any
+    other uppercase English character. You can perform this operation at most k times.
+    Return the length of the longest substring containing the same letter you can get after performing the
+    above operations.
+
+    
+    console.log(longest_repeating_char_replace("ABAB",2)) -> 4
+    console.log(longest_repeating_char_replace("AABABBA", 1)) -> 4
+
+
+  */
+};
